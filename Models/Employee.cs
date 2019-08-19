62f67d011c749c38de2839ba50c4e89ee92891d7
ryscottarmstrong <ryscottarmstrong@gmail.com>
2019-08-19 08:58:15 -0600

    Created the base class, tried to create a child class of Employee, not sure why it couldn't find the reference.

---
+++ Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.CSharp;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string EmailAddress { get; set; }
+    public string Preferred_Contact_Number { get; set; }
+    public string Position { get; set; }
+    public Dictionary<int, string> Department { get; set; }
+    public DateTime StartDate { get; set; }
+    public bool EmploymentStatus { get; set; }
+    public DateTime TerminationDate { get; set; }
+    public enum Shift
+    {
+        Morning, Afternoon, Evening, Night, Graveyard
+    }
+
+   
+
+    /*
+·  Shift
+
+·  Manager
+
+·  Team Member Photo
+
+·  Favorite Color
+*/
+    public Employee()
+	{
+	}
+}
